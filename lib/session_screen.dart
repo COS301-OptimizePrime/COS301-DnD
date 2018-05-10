1ce99e82f8e6633be122a9788dc9d44d6f8da1df
Thomas Scholtz <thomas@quantum-sicarius.za.net>
2018-05-10 20:49:24 +0200

    added ability to scroll to refresh session.

--- lib/session_screen.dart
+++ lib/session_screen.dart
@@ -2,13 +2,60 @@ import 'package:flutter/material.dart';
 import 'backend/server.pb.dart';
 import 'backend/server.pbgrpc.dart';
 import 'package:dnd_301_final/menu.dart';
+import 'dart:async';
 
+import 'package:dnd_301_final/app_data.dart';
 
-class GameSessionDemo extends StatelessWidget {
+class GameSessionDemo extends StatefulWidget {
   static String tag = "game-session";
 
-  GameSessionDemo(this.session);
   final Session session;
+
+  GameSessionDemo(this.session) {
+    /*for (User user in session.users) {
+      print(user.name);
+      _items.add('User: ' + user.name);
+    }*/
+  }
+  @override
+  GameSessionState createState() => new GameSessionState(this.session);
+
+}
+
+class GameSessionState extends State<GameSessionDemo> {
+
+  static List<String> _items = <String>[];
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = new GlobalKey<RefreshIndicatorState>();
+  final Session session;
+
+
+  GameSessionState(this.session) {
+    _items = <String>['Welcome to ${this.session.name}','Dungeon master: ${this.session.dungeonMaster.name}'];
+  }
+
+
+  Future<Null> _handleRefresh() async{
+    GetSessionRequest gsr = new GetSessionRequest();
+    gsr.sessionId = this.session.sessionId;
+    gsr.authIdToken = AppData.token;
+
+    final response = await AppData.stub.getSessionById(gsr);
+    print('Status: ${response.status}');
+    print('Status Message: ${response.statusMessage}');
+
+    _items = <String>['Welcome to ${this.session.name}','Dungeon master: ${this.session.dungeonMaster.name}'];
+
+    for (User user in session.users) {
+      print(user.name);
+      _items.add('User: ' + user.name);
+    }
+    setState((){
+      _items = _items;
+    });
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +64,24 @@ class GameSessionDemo extends StatelessWidget {
       appBar: new AppBar( //AppBars are the bars on top of the view
         title: const Text('Current Session Page'),
       ),
-      body: new Center(
-        child: new ListView(
-          controller: new ScrollController(),
-          shrinkWrap: true,
-          padding: new EdgeInsets.only(left: 24.0, right: 24.0,top: 50.0),
-          children: <Widget>[
-            new SizedBox(height: 48.0),
-            new Container(child : new Text("Welcome to ${this.session.name} session!",overflow: TextOverflow.ellipsis,maxLines: 7,textAlign: TextAlign.center,)),
-            new Container(child : new Text("This session has: ${this.session.dungeonMaster.name} as the dungeon master!",overflow: TextOverflow.ellipsis,maxLines: 7,textAlign: TextAlign.center,)),
-            new Container(child : new Text("Current players in this session: -${this.session.users}",overflow: TextOverflow.ellipsis,maxLines: 7,textAlign: TextAlign.center,)),
-          ],
+      body: new RefreshIndicator(
+        key: _refreshIndicatorKey,
+        onRefresh: _handleRefresh,
+        child: new Center(
+            child: new ListView.builder(
+                padding: kMaterialListPadding,
+                itemCount: _items.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final String item = _items[index];
+                  return new ListTile(
+                    isThreeLine: true,
+                    title: new Text('$item.'),
+                    subtitle: const Text(
+                        'Even more additional list item information appears on line three.'),
+                  );
+                })
+          ),
         ),
-      ),
-    );
+      );
   }
 }
